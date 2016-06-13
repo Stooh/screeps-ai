adcd45baa39c1e3c2b9aaf22e8010205d17383f8
Stoh <sim.nowak@gmail.com>
2016-06-13 02:02:34 +0200

    able to get game objects too

--- BTGetGameStack.js
+++ BTGetGameStack.js
@@ -10,7 +10,9 @@ module.exports = require('BTTaskLeaf').extend({
     tick: function(executor, context) {
         var stack = Game[this.source];
 
-        if(!_.isArray(stack)) {
+        if(_.isObject)
+            stack = _.values(stack);
+        else if(!_.isArray(stack)) {
             Log.warn('Stack is not an array: ' + stack);
             return BTTask.FAILURE;
         }
